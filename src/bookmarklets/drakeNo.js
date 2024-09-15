52f96f4df669e9721b311f413787ddb0ba5f0527
Joseph Richardson <EHLOVader@users.noreply.github.com>
2024-09-15 18:29:45 -0400

    drake no meme

---
+++ src/bookmarklets/drakeNo.js
@@ -0,0 +1,19 @@
+const memeSpec = {
+    id: "drakeNo",
+    elements: [
+        {
+            element: "img",
+            attributes: {
+                "id": "drakeNo",
+                "src": "https://i.imgur.com/5bjP6RE.png",
+                "alt": "NO!",
+                "style": {
+                    "position": "fixed",
+                    "left": "0",
+                    "bottom": "0",
+                    "zIndex": "1000000"
+                }
+            }
+        }
+    ]
+}
