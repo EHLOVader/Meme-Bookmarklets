aa931f0c5a2e5bf6c37bca585d378f129cc5a593
Joseph Richardson <EHLOVader@users.noreply.github.com>
2024-09-15 16:59:34 -0400

    Improved soyjak

--- src/bookmarklets/soyjak.js
+++ src/bookmarklets/soyjak.js
@@ -1,62 +1,92 @@
-const settings = {
-  "images": [
-    {
-      "id": "soyjakLeft",
-      "src": "https://i.imgur.com/IKKabUR.png",
-      "position": "left",
-      "alt": "WTF!",
-      "style": {
-        "position": "fixed",
-        "left": "0",
-        "bottom": "0",
-        "zIndex": "1000000",
-        "maxHeight": "100%",
-        "maxWidth": "40%"
-      }
-    },
-    {
-      "id": "soyjakRight",
-      "src": "https://i.imgur.com/Njv3g2b.png",
-      "position": "right",
-      "alt": "WTF!",
-      "style": {
-        "position": "fixed",
-        "right": "0",
-        "bottom": "0",
-        "zIndex": "1000000",
-        "maxHeight": "100%",
-        "maxWidth": "40%"
-      }
-    }
-  ]
+const meme =
+{
+    id: 'soyjak',
+    elements: [
+        {
+            element: 'div',
+            attributes: {
+                id: 'soyjak',
+                style: {
+                    position: 'fixed',
+                    top: '0',
+                    left: '0',
+                    right: '0',
+                    bottom: '0',
+                    zIndex: '1000000',
+                    pointerEvents: 'none'
+                }
+            },
+        },
+        {
+            element: 'img',
+            attributes: {
+                src: 'https://i.imgur.com/IKKabUR.png',
+                alt: 'WTF!',
+                style: {
+                    position: 'fixed',
+                    left: 0,
+                    bottom: '0',
+                    zIndex: '1000000',
+                    maxHeight: '100%',
+                    maxWidth: '40%',
+                    pointerEvents: 'none'
+                },
+                onerror: function() {
+                    alert('Failed to load the image. It might be blocked due to CORS policy.');
+                    this.remove();
+                }
+            },
+            parent: 'soyjak'
+        },
+        {
+            element: 'img',
+            attributes: {
+                src: 'https://i.imgur.com/Njv3g2b.png',
+                alt: 'WTF!',
+                style: {
+                    position: 'fixed',
+                    right: '0',
+                    bottom: '0',
+                    zIndex: '1000000',
+                    maxHeight: '100%',
+                    maxWidth: '40%',
+                    pointerEvents: 'none'
+                },
+                onerror: function() {
+                    alert('Failed to load the image. It might be blocked due to CORS policy.');
+                    this.remove();
+                }
+            },
+            parent: 'soyjak'
+        }
+    ]
 };
 
-function createImageElement(settings) {
-  const img = document.createElement('img');
-  img.setAttribute('id', settings.id);
-  img.src = settings.src;
-  img.alt = settings.alt;
-  Object.assign(img.style, settings.style);
-  img.onerror = function() {
-    alert('Failed to load the image. It might be blocked due to CORS policy.');
-    this.remove();
-  };
-  return img;
+function createMeme(meme) {
+    const elements = meme.elements;
+    elements.forEach(element => {
+        const el = document.createElement(element.element);
+        const attributes = element.attributes;
+        Object.keys(attributes).forEach(key => {
+            if (key === 'style') {
+                Object.assign(el.style, attributes[key]);
+            } else {
+                el[key] = attributes[key];
+            }
+        });
+        if (element.parent) {
+            const parent = document.getElementById(element.parent);
+            parent.appendChild(el);
+        } else {
+            document.body.appendChild(el);
+        }
+    });
 }
 
-function toggleImage(id) {
-  const imgElement = document.getElementById(id);
-  if (imgElement) {
-    imgElement.remove();
-  } else {
-    const settings = settings.images.find(image => image.id === id);
-    if (settings) {
-      const img = createImageElement(settings);
-      document.body.appendChild(img);
+
+    let soyjak = document.getElementById('soyjak');
+    if (soyjak) {
+        soyjak.remove();
+    } else {
+        createMeme(meme);
     }
-  }
-}
-
-// Example usage
-toggleImage('soyjakLeft');
-toggleImage('soyjakRight');
