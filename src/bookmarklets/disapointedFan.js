0482ab5a389f887013f1a61e708da538fa4e6120
Joseph Richardson <EHLOVader@users.noreply.github.com>
2024-09-19 09:11:42 -0400

    Clean up json to avoid errors and improve parsing

--- src/bookmarklets/disapointedFan.js
+++ src/bookmarklets/disapointedFan.js
@@ -1,9 +1,9 @@
 const memeSpec = {
-    id: "disapointedFan",
-    elements: [
+    "id": "disapointedFan",
+    "elements": [
         {
-            element: "img",
-            attributes: {
+            "element": "img",
+            "attributes": {
                 "id": "disapointedFan",
                 "src": "https://i.imgur.com/COnoKvI.png",
                 "alt": "Really!?",
@@ -12,7 +12,7 @@ const memeSpec = {
                     "right": "0",
                     "bottom": "0",
                     "zIndex": "1000000",
-                    "pointerEvents": "none",
+                    "pointerEvents": "none"
                 }
             }
         }
