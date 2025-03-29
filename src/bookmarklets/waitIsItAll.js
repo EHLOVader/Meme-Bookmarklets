5ebe3e05d108870599ba456a878c15750dc5e955
Joseph Richardson <EHLOVader@users.noreply.github.com>
2025-03-29 18:23:34 -0400

    :bug: Fix the wait is it all meme
    Fixes corner, uses new image with missing astronaut butt, and adjusts text for mobile

--- src/bookmarklets/waitIsItAll.js
+++ src/bookmarklets/waitIsItAll.js
@@ -20,14 +20,16 @@ const memeSpec = {
             "element": "img",
             "attributes": {
                 "id": "waitIsItAllImg",
-                "src": "https://i.imgur.com/sZ0efyn.png",
+                "src": "https://i.imgur.com/zuy1yDW.png",
                 "alt": "It always has been",
                 "style": {
                     "position": "fixed",
                     "right": "0",
                     "bottom": "0",
                     "zIndex": "1000000",
-                    "pointerEvents": "none"
+                    "pointerEvents": "none",
+                    "max-width": "100%",
+                    "min-width": "460px"
                 }
             },
             "parent": "waitIsItAll"
@@ -39,14 +41,18 @@ const memeSpec = {
                 "textContent": "Wait, is it all...",
                 "style": {
                     "position": "fixed",
-                    "right": "400px",
-                    "bottom": "270px",
+                    "right": "min(400px, 40vw)",
+                    "bottom": "min(270px, 30vw)",
                     "zIndex": "1000000",
                     "pointerEvents": "none",
                     "color": "white",
-                    "fontFamily": "Arial",
+                    "fontFamily": "Impact, Arial, sans-serif",
                     "fontSize": "2em",
                     "padding": "1em",
+                    "white-space": "nowrap",
+                    "-webkit-text-stroke": "2px #000",
+                    "-webkit-text-fill-color": "#fff",
+                    "letter-spacing": "0.02em",
                     "textShadow": "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000"
                 }
             },
@@ -60,13 +66,17 @@ const memeSpec = {
                 "style": {
                     "position": "fixed",
                     "right": "10px",
-                    "bottom": "430px",
+                    "bottom": "min(450px, 48vw)",
                     "zIndex": "1000000",
                     "pointerEvents": "none",
                     "color": "white",
-                    "fontFamily": "Arial",
+                    "fontFamily": "Impact, Arial, sans-serif",
                     "fontSize": "2em",
                     "padding": "1em",
+                    "white-space": "nowrap",
+                    "-webkit-text-stroke": "2px #000",
+                    "-webkit-text-fill-color": "#fff",
+                    "letter-spacing": "0.02em",
                     "textShadow": "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000"
                 }
             },
